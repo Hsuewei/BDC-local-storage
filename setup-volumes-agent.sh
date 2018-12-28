2910da08fa60847d874ba5b9b3831ed6fca194da
asktiger <ask_tiger@hotmail.com>
2018-12-28 09:48:35 +0800

    Initial commit

---
+++ setup-volumes-agent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+# num of persistent volumes
+PV_COUNT=25
+
+for i in $(seq 1 $PV_COUNT); do
+  vol="vol$i"
+  
+  mkdir -p /mnt/nvme/$vol
+  mount --bind /mnt/nvme/$vol /mnt/nvme/$vol
+  mkdir -p /mnt/ssd/$vol
+  mount --bind /mnt/ssd/$vol /mnt/ssd/$vol
+  mkdir -p /mnt/hdd/$vol
+  mount --bind /mnt/hdd/$vol /mnt/hdd/$vol
+done
