23d743325a8833b6b0cac7245fc273bade4e6864
asktiger <ask_tiger@hotmail.com>
2019-05-14 10:55:12 +0800

    Update setup-volumes-agent.sh

--- setup-volumes-agent.sh
+++ setup-volumes-agent.sh
@@ -6,12 +6,23 @@ PV_COUNT=25
 for i in $(seq 1 $PV_COUNT); do
   vol="vol$i"
 
-  mkdir -p /mnt/pmem/$vol
-  mount --bind /mnt/pmem/$vol /mnt/pmem/$vol
-  mkdir -p /mnt/nvme/$vol
-  mount --bind /mnt/nvme/$vol /mnt/nvme/$vol
-  mkdir -p /mnt/ssd/$vol
-  mount --bind /mnt/ssd/$vol /mnt/ssd/$vol
-  mkdir -p /mnt/hdd/$vol
-  mount --bind /mnt/hdd/$vol /mnt/hdd/$vol
+if  [ /dev/mapper/db_nvme-vol ] &&  [ /dev/mapper/db_ssd-vol] &&  [ /dev/mapper/db_hdd-vol]; then
+    mkdir -p /mnt/nvme/$vol
+    mount --bind /mnt/nvme/$vol /mnt/nvme/$vol
+    mkdir -p /mnt/ssd/$vol
+    mount --bind /mnt/ssd/$vol /mnt/ssd/$vol
+    mkdir -p /mnt/hdd/$vol
+    mount --bind /mnt/hdd/$vol /mnt/hdd/$vol
+
+elif  [ /dev/mapper/db_pmem-vol ] &&  [ /dev/mapper/db_ssd-vol]; then
+    mkdir -p /mnt/pmem/$vol
+    mount --bind /mnt/pmem/$vol /mnt/pmem/$vol
+    mkdir -p /mnt/ssd/$vol
+    mount --bind /mnt/ssd/$vol /mnt/ssd/$vol
+
+else 
+    mkdir -p /mnt/local-storage
+    mount --bind /mnt/local-storage/$vol /mnt/local-storage/$vol
+fi
+
 done
