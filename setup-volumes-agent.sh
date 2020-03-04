7ebb6afe968460e8099568e95a2c3d71c298a86e
asktiger <ask_tiger@hotmail.com>
2020-03-04 09:40:26 +0800

    Update setup-volumes-agent.sh

--- setup-volumes-agent.sh
+++ setup-volumes-agent.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
 #Num of persistent volumes
-PV_COUNT=25
+PV_COUNT=30
 
 for i in $(seq 1 $PV_COUNT); do
 vol=vol$i
 
-if  [ /dev/mapper/db_nvme-vol -a /dev/mapper/db_ssd-vol -a /dev/mapper/db_hdd-vol ]; then
+if  [ -L /dev/mapper/db_nvme-vol ] && [ -L /dev/mapper/db_ssd-vol ] && [ -L /dev/mapper/db_hdd-vol ]
+then
     mkdir -p /mnt/nvme/$vol
     mount --bind /mnt/nvme/$vol /mnt/nvme/$vol
     mkdir -p /mnt/ssd/$vol
@@ -14,13 +15,14 @@ if  [ /dev/mapper/db_nvme-vol -a /dev/mapper/db_ssd-vol -a /dev/mapper/db_hdd-vo
     mkdir -p /mnt/hdd/$vol
     mount --bind /mnt/hdd/$vol /mnt/hdd/$vol
 
-elif  [ /dev/mapper/db_pmem-vol -a /dev/mapper/db_ssd-vol ]; then
+elif  [ -L /dev/mapper/db_pmem-vol ] && [ -L /dev/mapper/db_ssd-vol ]
+then
     mkdir -p /mnt/pmem/$vol
     mount --bind /mnt/pmem/$vol /mnt/pmem/$vol
     mkdir -p /mnt/ssd/$vol
     mount --bind /mnt/ssd/$vol /mnt/ssd/$vol
 
-elif [ ! /dev/mapper/db_nvme-vol -o ! /dev/mapper/db_ssd-vol -o ! /dev/mapper/db_hdd-vol -o ! /dev/mapper/db_pmem-vol ]; then
+else
     mkdir -p /mnt/local-storage/$vol
     mount --bind /mnt/local-storage/$vol /mnt/local-storage/$vol
 fi
